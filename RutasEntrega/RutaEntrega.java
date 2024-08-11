e95ffa4f272da05a1fb1b48c5abc74c2ac74ab40
Fernando.Helio <febolivarbr@est.utn.ac.cr>
2024-08-11 16:28:52 -0600

    Clase RutaEntrega
    Se creo la clase RutaEntrega con todos sus atributos correspondientes, sus metodos get y sus constructores

---
+++ RutasEntrega/RutaEntrega.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RutasEntrega;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Fernando
+ */
+public class RutaEntrega {
+    
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private HashSet<String> listaDestinos ;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public HashSet<String> getListaDestinos() {
+        return listaDestinos;
+    }
+
+    public RutaEntrega(String codigo, String nombre, String descripcion, HashSet<String> listaDestinos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.listaDestinos = listaDestinos;
+    }
+    
+    public RutaEntrega() {
+        this("","","",new HashSet<>());
+    }
+
+    @Override
+    public String toString() {
+        return "RutaEntrega{" + "codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + ", listaDestinos=" + listaDestinos + '}';
+    }
+    
+    
+    
+    
+}
