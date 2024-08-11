a6bcbe1891e74dc7312c9933e2cf9d66b277f45f
TodV <d2tod12@gmail.com>
2024-08-10 20:55:42 -0600

    Empieza el proyecto
    Se crear la interfaz Lista y los packs de cada conjunto de clases. Para más informacion consulte con su Derek de confianza.

---
+++ Listas/Lista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Listas;
+
+
+/**
+ *
+ * @author d2tod
+ * @param <Objeto>
+ */
+public interface Lista<Objeto> {
+    public void agregar(Objeto objeto);
+    public void eliminar(int identificacion);
+}
